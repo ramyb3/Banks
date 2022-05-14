8378e8b74abf6e4aba8fd7b7ee8057a143dfd1a0
Ramy Bachayev <90751345+ramyb3@users.noreply.github.com>
2022-05-14 14:45:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ export const getData = async (location) => // get data about banks
 
 export const getLocation= async (position)=> // get location using google maps api
 {
-    const key='AIzaSyBCdUC8KHIIIfQLIfUHQOnPwzpwFPKqt_U'; // api key
+    const key='<PUT YOUR API KEY HERE>'; // api key
 
     let temp= await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${position[0]},${position[1]}&key=${key}`);
     return temp.data.results[0].address_components.find(x=> x.types.includes('locality') && x.types.includes('political')).long_name; //get user's city    
