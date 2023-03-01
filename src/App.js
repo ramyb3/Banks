eb7f2f74669ac5d15c669e3a78b0f6550c87d3e5
ramy <ramy_3@walla.com>
2023-03-01 12:47:53 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import "./App.css";
+import Bank from "./bank";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import emailjs from "emailjs-com";
+
+export default function App() {
+  const [banks, setBanks] = useState([]);
+  const [city, setCity] = useState("");
+
+  // check permission to user location
+  useEffect(() => {
+    window.navigator.permissions &&
+      window.navigator.permissions
+        .query({ name: "geolocation" })
+        .then(async function (PermissionStatus) {
+          if (
+            PermissionStatus.state === "granted" ||
+            PermissionStatus.state === "prompt"
+          ) {
+            window.navigator.geolocation.getCurrentPosition(
+              async (position) => {
+                const location = await getLocation([
+                  position.coords.latitude,
+                  position.coords.longitude,
+                ]);
+                const list = await getData(location);
+
+                setBanks(list);
+              }
+            );
+          }
+        });
+
+    const templateParams = {
+      message: `banks:\n${navigator.userAgent};\nresolution: ${window.screen.width} X ${window.screen.height}`,
+    };
+
+    emailjs.send(
+      process.env.REACT_APP_EMAIL_JS_SERVICE,
+      process.env.REACT_APP_EMAIL_JS_TEMPLATE,
+      templateParams,
+      process.env.REACT_APP_EMAIL_JS_USER
+    );
+  }, []);
+
+  const getBanks = async () => {
+    if (city != "") {
+      const list = await getData(city);
+      setBanks(list);
+
+      if (list.length == 0) {
+        alert("אין תוצאות! נסו שוב");
+      }
+    } else {
+      alert("נא להקליד עיר בשביל לקבל תוצאה!");
+    }
+  };
+
+  return (
+    <>
+      <h1>כל סניפי הבנקים שבעיר שלך</h1>
+
+      {banks.length > 0 ? (
+        <div className="container">
+          {banks.map((bank, index) => {
+            return (
+              <div className="card" key={index}>
+                <Bank bank={bank} />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <>
+          <input
+            placeholder="הזן עיר"
+            type="text"
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button onClick={getBanks}>חפש</button>
+        </>
+      )}
+    </>
+  );
+}
+
+// get data about banks
+export const getData = async (location) => {
+  let resp = await axios.get(
+    "https://data.gov.il/api/3/action/datastore_search?resource_id=1c5bc716-8210-4ec7-85be-92e6271955c2&&limit=0"
+  );
+
+  const total = resp.data.result.total; //get all israel banks
+
+  resp = await axios.get(
+    `https://data.gov.il/api/3/action/datastore_search?resource_id=1c5bc716-8210-4ec7-85be-92e6271955c2&&limit=${total}`
+  );
+
+  //returns all banks in the city
+  return resp.data.result.records.filter(
+    (x) =>
+      x.City.replace(/\s/g, "").includes(location.replace(/\s/g, "")) &&
+      x.Date_Closed == ""
+  );
+};
+
+// get location using google maps api
+export const getLocation = async (position) => {
+  const key = process.env.REACT_APP_API_KEY; // api key
+
+  const temp = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${position[0]},${position[1]}&key=${key}`
+  );
+
+  return temp.data.results[0].address_components.find(
+    (x) => x.types.includes("locality") && x.types.includes("political")
+  ).long_name; //get user's city
+};
---
+++ src/bank.js
@@ -0,0 +1,33 @@
+export default function Bank(props) {
+  return (
+    <>
+      <div className="name">{props.bank.Bank_Name}</div>
+      <div className="name branch">{props.bank.Branch_Name}</div>
+      <div className="address">{props.bank.Branch_Address}</div>
+
+      {props.bank.day_closed != "" ? (
+        <div className="close">סגור ב{props.bank.day_closed}'</div>
+      ) : null}
+
+      <div className="address type">סוג סניף: {props.bank.Branch_Type}</div>
+
+      <a
+        href={`https://www.google.co.il/maps/search/${props.bank.City} ${props.bank.Branch_Address}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="https://www.pngplay.com/wp-content/uploads/12/GPS-Icon-PNG-Clip-Art-HD-Quality.png" />
+      </a>
+
+      {props.bank.Telephone != "" ? (
+        <a href={"tel:" + props.bank.Telephone}>
+          <img src="https://www.seekpng.com/png/detail/814-8147045_call-us-phone-icon-black-circle.png" />
+        </a>
+      ) : null}
+
+      {props.bank.Handicap_Access == "כן" ? (
+        <img src="https://www.nicepng.com/png/detail/119-1192777_assistive-technology-icon-accessibility-icon-white.png" />
+      ) : null}
+    </>
+  );
+}
